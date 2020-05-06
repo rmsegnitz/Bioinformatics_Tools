bf6c44da2d2c31a35d7ea63c5f9e01a208bd6268
rmsegnitz <43895313+rmsegnitz@users.noreply.github.com>
2020-05-06 11:32:01 -0700

    Create extractMediationSummary() script.
    Create separate script for the extractMediationSummary() function, which extracts a matrix class summary of mediation analysis run by mediate() in the package "mediation".

---
+++ R_functions/extractMediationSummary.R
@@ -0,0 +1,68 @@
+# Mediation Analysis for Gene Sets 
+# Author: Max Segnitz, msegnitz@uw.edu
+# Started April 2020
+#
+# License: This software is licensed under GNU General Public License, and may
+# be modified and/or redistributed under GNU GPL version 3 or later. License details
+# can be found in the accompanying this script, or at  (http://www.gnu.org/licenses/).
+#
+# DESCRIPTION:
+# extractMediationSummary() is a function to extract mediation summary from mediation() function
+# in the {mediate} package in matrix format.
+#
+
+## This function comes from 
+## [https://stackoverflow.com/questions/41582486/how-to-convert-r-mediation-summary-to-data-frame ] 
+## and is not my own. It has been modified in the name of the function only.
+## Original Author: Stack Overflow user "hrbrmstr"
+## This material is covered under Creative Commons,compatible with GNU GLP v3. 
+
+### DEFINE FUNCTION ####
+
+extractMediationSummary <- function (x) { 
+  
+  clp <- 100 * x$conf.level
+  isLinear.y <- ((class(x$model.y)[1] %in% c("lm", "rq")) || 
+                   (inherits(x$model.y, "glm") && x$model.y$family$family == 
+                      "gaussian" && x$model.y$family$link == "identity") || 
+                   (inherits(x$model.y, "survreg") && x$model.y$dist == 
+                      "gaussian"))
+  
+  printone <- !x$INT && isLinear.y
+  
+  if (printone) {
+    
+    smat <- c(x$d1, x$d1.ci, x$d1.p)
+    smat <- rbind(smat, c(x$z0, x$z0.ci, x$z0.p))
+    smat <- rbind(smat, c(x$tau.coef, x$tau.ci, x$tau.p))
+    smat <- rbind(smat, c(x$n0, x$n0.ci, x$n0.p))
+    
+    rownames(smat) <- c("ACME", "ADE", "Total Effect", "Prop. Mediated")
+    
+  } else {
+    smat <- c(x$d0, x$d0.ci, x$d0.p)
+    smat <- rbind(smat, c(x$d1, x$d1.ci, x$d1.p))
+    smat <- rbind(smat, c(x$z0, x$z0.ci, x$z0.p))
+    smat <- rbind(smat, c(x$z1, x$z1.ci, x$z1.p))
+    smat <- rbind(smat, c(x$tau.coef, x$tau.ci, x$tau.p))
+    smat <- rbind(smat, c(x$n0, x$n0.ci, x$n0.p))
+    smat <- rbind(smat, c(x$n1, x$n1.ci, x$n1.p))
+    smat <- rbind(smat, c(x$d.avg, x$d.avg.ci, x$d.avg.p))
+    smat <- rbind(smat, c(x$z.avg, x$z.avg.ci, x$z.avg.p))
+    smat <- rbind(smat, c(x$n.avg, x$n.avg.ci, x$n.avg.p))
+    
+    rownames(smat) <- c("ACME (control)", "ACME (treated)", 
+                        "ADE (control)", "ADE (treated)", "Total Effect", 
+                        "Prop. Mediated (control)", "Prop. Mediated (treated)", 
+                        "ACME (average)", "ADE (average)", "Prop. Mediated (average)")
+    
+  }
+  
+  colnames(smat) <- c("Estimate", paste(clp, "% CI Lower", sep = ""), 
+                      paste(clp, "% CI Upper", sep = ""), "p-value")
+  smat
+  
+}
+
+
+
